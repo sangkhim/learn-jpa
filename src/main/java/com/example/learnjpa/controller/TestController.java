53d74f7e73f3f366a6bff26156c096bf96eec076
sangkhim <sangkhimpay@gmail.com>
2023-09-29 21:35:02 +0700

    update

--- src/main/java/com/example/learnjpa/controller/TestController.java
+++ src/main/java/com/example/learnjpa/controller/TestController.java
@@ -4,6 +4,7 @@ import com.example.learnjpa.entity.Address;
 import com.example.learnjpa.entity.Course;
 import com.example.learnjpa.entity.Student;
 import com.example.learnjpa.entity.base.Auditor;
+import com.example.learnjpa.entity.enums.Sex;
 import com.example.learnjpa.repository.StudentRepository;
 import java.util.Arrays;
 import java.util.Date;
@@ -13,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +35,7 @@ public class TestController {
   public ResponseEntity<Void> save() {
     Student student = new Student();
     student.setName("admin");
+    student.setSex(Sex.FEMALE);
 
     Auditor auditor = new Auditor();
     auditor.setCreatedDate(new Date());
@@ -49,6 +52,30 @@ public class TestController {
     course2.setName("Math");
 
     student.setCourses(Arrays.asList(course1, course2));
+
+    studentRepository.save(student);
+
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  @PostMapping("/update/{studentId}")
+  public ResponseEntity<Void> update(@PathVariable("studentId") Long studentId) {
+    Student student = studentRepository.findById(studentId).get();
+    student.setName("admin update");
+    student.setSex(Sex.MALE);
+
+    Auditor auditor = new Auditor();
+    auditor.setCreatedDate(new Date());
+    auditor.setCreatedBy("operator");
+    student.setAuditor(auditor);
+
+    Address address = student.getAddress();
+    address.setAddress("#95, st. 608 update");
+    student.setAddress(address);
+
+    student.getCourses().forEach(course -> {
+      course.setName(course.getName() + " update");
+    });
 
     studentRepository.save(student);
 
